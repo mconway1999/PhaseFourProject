ed2ee43c131c85708a9527c9bde5c1b9231f6bad
Madeline Conway <conway.maddy@yahoo.com>
2024-11-12 10:26:21 -0500

    made changes

--- server/app.py
+++ server/app.py
@@ -1,17 +1,74 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-
+from flask import Flask, make_response, request
 # Remote library imports
 from flask import request
 from flask_restful import Resource
-
+from flask_migrate import Migrate
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI']
+db.init_app(app) 
+api = Api(app)
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import CoffeeShop, Reviews, Customers, Coupons
 
 # Views go here!
+class Reviews:
+    def post(self):
+        review_data = request.json.get('review')
+        stars_data = request.json.get('stars')
+        try:
+            new_review = Reviews(stars=stars_data, review=review_data)
+            db.session.add(new_review)
+            db.session.commit()
+            response_body = new_review.to_dict(only=('id', 'stars', 'review'))
+            return make_response(response_body, 201)
+        except:
+            response_body = {
+                "error": "please enter a valid review!"
+             }
+            return make_response(response_body, 422)
+    api.add_resource()
+
+    def patch(self):
+        review= db.session.get(Reviews)
+        if review:
+                for attr in request.json:
+                    setattr(review, attr, request.json.get(attr))
+                db.session.commit()
+                response_body = review.to_dict(only=('review', 'stars', 'id'))
+                return make_response(response_body, 200)
+        else:
+                response_body = {
+                    "error": "Please write a valid review!"
+                }
+                return make_response(response_body, 422)
+    api.add_resource()
+        
+    def delete(self):
+        review= db.session.get(Reviews)
+        if review:
+            db.session.delete(review)
+            db.session.commit()
+            return make_response({}, 204)
+        
+        else:
+            response_body = {
+                "error": "Unable to delete review"
+            }
+            return make_response(response_body, 404)
+    api.add_resource()
+
+class Coupons:
+     def get(self):
+        coupons = Coupons.query.all()
+        response_body = [coupon.to_dict(only=('id', 'coupon_comment')) for coupon in coupons]
+        return make_response(response_body, 200)
+     api.add_resource()
+
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -1,6 +1,76 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
-# Models go here!
+class CoffeeShop(db.Model, SerializerMixin):
+     __tablename__ ='coffee_shops'
+     id = db.Column(db.Integer, primary_key=True)
+     image = db.Column(db.String, nullable=False)
+     name = db.Column(db.String, nullable=False)
+
+     coupons = db.relationship('Coupons', backpopulates = 'coffee_shop')
+     reviews = db.relationship('Reviews', back_populates = 'coffee_shop')
+
+     @validates('name')
+     def validate_review(self, column, value):
+        if type(value) == str:
+            return value
+        else:
+            raise Exception(f"{column} must be a string")
+
+class Coupons(db.Model, SerializerMixin):
+     __tablename__ ='coupons'
+     id = db.Column(db.Integer, primary_key=True)
+     coupon_comment = db.Column(db.String, nullable=False)
+     coffee_shop_id=db.Column(db.Integer, nullable=False)
+    
+     coffee_shop = db.relationship('CoffeeShop', backpopulates='coupons')
+
+     @validates('coupon_comment')
+     def validate_review(self, column, value):
+        if type(value) == str and len(value) >= 10:
+            return value
+        else:
+            raise Exception(f"{column} must be a string and it must be at least 10 characters long")
+
+class Customers(db.Model, SerializerMixin):
+     __tablename__ ='customers'
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String, nullable=False)
+     
+     reviews = db.relationship('Reviews', back_populates = 'customer')
+
+     @validates('name')
+     def validate_name(self, column, value):
+        if type(value) == str:
+            return value
+        else:
+            raise Exception(f"{column} must be a string")
+
+class Reviews(db.Model, SerializerMixin):
+     __tablename__ ='reviews'
+     id = db.Column(db.Integer, primary_key=True)
+     review = db.Column(db.String, nullable=False)
+     stars = db.Column(db.Integer, nullable=False)
+
+     customer_id=db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+     coffee_shop_id=db.Column(db.Integer, db.ForeignKey('coffee_shops.id'), nullable=False)
+
+     customer = db.relationship('Customer', back_populates="reviews")
+     coffee_shop = db.relationship('CoffeeShop', back_populates="reviews")
+
+
+     @validates('review')
+     def validate_review(self, column, value):
+        if type(value) == str and len(value) >= 8:
+            return value
+        else:
+            raise Exception(f"{column} must be a string and it must be at least 8 characters long")
+        
+     @validates('stars')
+     def validate_stars(self, column, value):
+        if type(value) == int and 1 <= value <= 5:
+            return value
+        raise Exception(f"{column} must be an integer between 1 and 5")
